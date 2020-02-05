52aaad13e659de4722c22bd929b80e3b4c41684e
SkyDoor-EvilSpirit <553134260@qq.com>
2020-02-05 19:26:05 +0800

    所有页面都以完成，jquery和bootstrap已配置，RESTful以配置成功，唯独还没有连接真正的数据库

--- spring-boot-crud/src/main/java/com/duyi/controller/ThymeleafCrudController.java
+++ spring-boot-crud/src/main/java/com/duyi/controller/ThymeleafCrudController.java
@@ -5,49 +5,58 @@ import com.duyi.service.GuestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @Controller
+@RequestMapping("/guest")
 public class ThymeleafCrudController {
 
     @Autowired
     private GuestService service;
 
-    @RequestMapping("/guest/list")
+//    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public String list(Model model){
         List<Guest> list = service.list();
         model.addAttribute("list",list);
         System.out.println("到list了。。。");
         return "view/index";
     }
-    @RequestMapping("/guest/addview")
+//    @RequestMapping(path = "addview")
+    @GetMapping("/add")
     public String addview(){
-        System.out.println("到addview了。。。");
+        System.out.println("到add了。。。");
         return "view/addview";
     }
-    @RequestMapping("/guest/add")
+//    @RequestMapping(path = "/guest",method =RequestMethod.POST)
+    @PostMapping
     public String add(Guest guest){
         service.add(guest);
         System.out.println("到add了。。。");
-        return "redirect:/guest/list";
+        return "redirect:/guest";
     }
-    @RequestMapping("/guest/updateview")
-    public String updateview(Model model,String name){
+//    @RequestMapping(path = "/guest/updateview",method = RequestMethod.GET)
+    @GetMapping("/update/{name}")
+    public String updateview(Model model,@PathVariable String name){
         Guest guest=service.get(name);
         model.addAttribute("guest",guest);
-        System.out.println(guest);
+        System.out.println("到update了。。。");
         return "view/update";
     }
-    @RequestMapping("/guest/update")
+//    @RequestMapping(path = "/guest/update/name",method = RequestMethod.PUT)
+    @PutMapping
     public String update(Guest newGuest){
+        System.out.println("到update的执行方法了...");
         service.update(newGuest);
-        return "redirect:/guest/list";
+        return "redirect:/guest";
     }
-    @RequestMapping("/guest/delete")
-    public String delete(String name){
+//    @RequestMapping(path = "/guest/delete/{name}",method = RequestMethod.DELETE)
+    @DeleteMapping("/{name}")
+    public String delete(@PathVariable String name){
         service.delete(name);
-        return "redirect:/guest/list";
+        return "redirect:/guest";
     }
 
 }
